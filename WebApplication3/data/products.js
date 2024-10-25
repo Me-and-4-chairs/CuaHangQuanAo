a2065f8306c7be09652d93026f1d3f9c40c933e5
ACER <ACER@DESKTOP-DV3F9RF>
2024-10-25 19:45:31 +0700

    Add project files.

---
+++ WebApplication3/data/products.js
@@ -0,0 +1,161 @@
+﻿var list_products = [{
+    "name": "Manchester United Shirt Home",
+    "company": "MU",
+    "img": "img/products/Mu1.jpg",
+    "price": "349.000",
+    "star": 5,
+    "rateCount": 99.934,
+    "promo": {
+        "name": "giareonlie",
+        "value": "300.000"
+    },
+    "detail": {
+        "chatlieu": "IPS LCD, 6.1', IPS LCD, 16 triệu màu",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Man United Store",
+    },
+    "masp": "Mu1"
+}, {
+    "name": "Manchester United Shirt Away Jersey",
+    "company": "MU",
+    "img": "img/products/Mu1k.jpg",
+    "price": "349.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "moiramat",
+        "value": "300.000"
+    },
+    "detail": {
+        "chatlieu": "IPS LCD, 6.1', IPS LCD, 16 triệu màu",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Man United Store",
+    },
+    "masp": "Mu2"
+}, {
+    "name": "Liverpool Shirt Home",
+    "company": "LVP",
+    "img": "img/products/Lvp1.jpg",
+    "price": "349.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "giareonline",
+        "value": "300.000"
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Liverpool Store",
+    },
+    "masp": "Lv1"
+}, {
+    "name": "Liverpool Shirt Away Jersey",
+    "company": "LVP",
+    "img": "img/products/Lvp1k.jpg",
+    "price": "349.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Liverpool Store",
+    },
+    "masp": "Lv2"
+}, {
+    "name": "Arsenal 1",
+    "company": "ARS",
+    "img": "img/products/Ars1.jpg",
+    "price": "300.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "250.000"
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Arsenal Store",
+    },
+    "masp": "Ars1"
+}, {
+    "name": "Juventus Shirt Home",
+    "company": "JV",
+    "img": "img/products/jv1.jpg",
+    "price": "299.000",
+    "star": 4,
+    "rateCount": 14,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Juventus Store",
+    },
+    "masp": "Ars2"
+}, {
+    "name": "Manchester City Shirt Home",
+    "company": "MC",
+    "img": "img/products/Mc1.jpg",
+    "price": "290.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "250.000"
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Manchester City Store",
+        "masp": "Mc1"
+    }
+}, {
+    "name": "Manchester City Shirt Away",
+    "company": "MC",
+    "img": "img/products/Mc1k.jpg",
+    "price": "290.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "250.000"
+    },
+    "detail": {
+        "chatlieu": "Cotton",
+        "form": "Người lớn",
+        "size": "Oversize",
+        "color": "Flexiable",
+        "tke": "Tối giản, co dãn, thoáng mát, hiện đại",
+        "dly": "Manchester City Store",
+        "masp": "Mc2"
+    }
+}]
